fbdc990b6a20e9b46f70996015d6e713649650fe
theepicCR <carrey455@gmail.com>
2025-06-09 14:14:15 -0500

    Added play.tsx UI

--- app/play.tsx
+++ app/play.tsx
@@ -1,10 +1,22 @@
 import React from "react";
-import { View, Text, StyleSheet, Image, Pressable } from "react-native";
+import { View, Text, StyleSheet, Image, Pressable, TextInput } from "react-native";
 import { standardStyles } from "../styles/styles";
 import { useRouter } from "expo-router";
+import { useState } from "react";
 
 export default function GamePage() {
     const router = useRouter();
+
+    let hint = "Hint";
+
+    const guessed = useState(false);
+    let topText = "Guess the Jalapeno!"
+    let questionNum = 1;
+
+    let width = 250;
+    let height = 200;
+
+
     return (
         <View style={standardStyles.container}>
             {/*Header*/}
@@ -32,10 +44,158 @@ export default function GamePage() {
                 </Text>
             </View>
 
+            <View style={styles.InstructionContainer}>
+                <Text style={styles.InstructionText}>{topText}</Text>
+            </View>
+
+            <View style={styles.contentContainer}>
+                <Image style={{height: height, width: width, alignSelf: "center",}}
+                    source={require("../assets/images/JalapenoBeeSil.png")}
+                    accessibilityLabel="It's Jalapeno Logo">
+                </Image>
+                <Pressable style={styles.hintButton}>
+                    <Text style={styles.hintButtonText}>
+                        {hint}
+                    </Text>
+                </Pressable>
+                <TextInput style={styles.guessEnter} 
+                    placeholder="Enter Guess:"
+                    placeholderTextColor={"#4A4A4A"}>
+                </TextInput>
+            </View>
+            <View style={styles.buttons}>
+                <View style={styles.questionNumContainer}>
+                    <Text style={styles.questionNumText}>
+                        {questionNum}
+                    </Text>
+                </View>
+                <Pressable style={styles.continueButton}>
+                    <Text style={styles.continueButtonText}>
+                        Next →
+                    </Text>
+                </Pressable>
+            </View>
+
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-
+    InstructionContainer: {
+    backgroundColor: "#FF79CA",
+    borderRadius: 20,
+    height: 75,
+    width: 600,
+    marginTop: 40,
+    alignSelf: "center",
+    justifyContent: "center",
+    borderColor: "#FF0099",
+    borderBottomWidth: 1,
+    borderTopWidth: 1,
+    borderLeftWidth: 1,
+    borderRightWidth: 1,
+  },
+  InstructionText: {
+    fontFamily: "JotiOne-Regular",
+    fontSize: 48,
+    textAlign: "center",
+    color: "#393939",
+  },
+  contentContainer: {
+    backgroundColor: "#FFFFFF",
+    borderRadius: 20,
+    height: 300,
+    width: 600,
+    marginTop: 30,
+    alignSelf: "center",
+    borderColor: "#FF0099",
+    borderBottomWidth: 2,
+    borderTopWidth: 2,
+    borderLeftWidth: 2,
+    borderRightWidth: 2,
+    padding: 20,
+    //justifyContent: "center",
+  },
+  hintButton: {
+    alignSelf: "center",
+    backgroundColor: "#FFF4AA",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 20,
+    borderColor: "#FFEE00",
+    borderBottomWidth: 2,
+    borderTopWidth: 2,
+    borderLeftWidth: 2,
+    borderRightWidth: 2,
+    paddingLeft: 5,
+    paddingRight: 5,
+    marginTop: 5,
+  },
+  hintButtonText: {
+    fontFamily: "JotiOne-Regular",
+    fontSize: 14,
+    textAlign: "center",
+    color: "#393939",
+  },
+  guessEnter: {
+    backgroundColor: "#D9D9D9",
+    width: 250,
+    height: 30,
+    alignSelf: "center",
+    marginTop: 5,
+    borderRadius: 10,
+    paddingLeft: 10,
+  },
+  guessEnterText: {
+    fontFamily: "JotiOne-Regular",
+    fontSize: 14,
+    textAlign: "center",
+    color: "#4A4A4A",
+  },
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginLeft: 30,
+    marginRight: 30,
+  },
+  questionNumContainer: {
+    backgroundColor: "#FF79CA",
+    borderRadius: 40,
+    height: 60,
+    width: 60,
+    marginTop: 30,
+    marginBottom: 30,
+    justifyContent: "center",
+    borderColor: "#FF0099",
+    borderBottomWidth: 1,
+    borderTopWidth: 1,
+    borderLeftWidth: 1,
+    borderRightWidth: 1,
+  },
+  questionNumText: {
+    fontFamily: "JotiOne-Regular",
+    fontSize: 30,
+    textAlign: "center",
+    color: "#393939",
+  },
+  continueButton: {
+    backgroundColor: "#FF79CA",
+    borderRadius: 20,
+    height: 60,
+    width: 120,
+    marginTop: 30,
+    marginBottom: 30,
+    justifyContent: "center",
+    borderColor: "#FF0099",
+    borderBottomWidth: 1,
+    borderTopWidth: 1,
+    borderLeftWidth: 1,
+    borderRightWidth: 1,
+  },
+  continueButtonText: {
+    fontFamily: "JotiOne-Regular",
+    fontSize: 24,
+    textAlign: "center",
+    color: "#393939",
+  }
 });
